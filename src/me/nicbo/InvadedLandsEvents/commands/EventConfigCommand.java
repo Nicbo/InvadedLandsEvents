1f9400a4cb90dd70e31c324bb398f731eea68762
Nicholas <nmac_100@hotmail.com>
2020-02-10 16:06:58 -0500

    null check started work on configedit

--- src/me/nicbo/InvadedLandsEvents/commands/EventConfigCommand.java
+++ src/me/nicbo/InvadedLandsEvents/commands/EventConfigCommand.java
@@ -7,15 +7,20 @@ import org.bukkit.configuration.file.FileConfiguration;
 
 public class EventConfigCommand implements CommandExecutor {
     private FileConfiguration config;
+    private String[] eventNames;
 
     public EventConfigCommand(FileConfiguration config) {
         this.config = config;
+        this.eventNames = EventManager.getEventNames();
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String s, String[] args) {
         if (cmd.getName().equalsIgnoreCase("eventconfig") || cmd.getName().equalsIgnoreCase("econfig")) {
-
+            if (args[0] == null) {
+                sender.sendMessage("todo help menu");
+                return true;
+            }
         }
         return false;
     }
