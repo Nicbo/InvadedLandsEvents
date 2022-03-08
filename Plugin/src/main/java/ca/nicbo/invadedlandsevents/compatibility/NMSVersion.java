9afc18b0a0c79080eb4842993bde45f5754056bd
Nicbo <43477837+Nicbo@users.noreply.github.com>
2022-03-07 19:34:19 -0500

    Bump spigot version

--- Plugin/src/main/java/ca/nicbo/invadedlandsevents/compatibility/NMSVersion.java
+++ Plugin/src/main/java/ca/nicbo/invadedlandsevents/compatibility/NMSVersion.java
@@ -19,7 +19,7 @@ public enum NMSVersion {
     v1_15_R1,
     v1_16_R1, v1_16_R2, v1_16_R3,
     v1_17_R1,
-    v1_18_R1;
+    v1_18_R1, v1_18_R2;
 
     private static final NMSVersion CURRENT_VERSION;
 
